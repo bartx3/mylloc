6f88e4dac9ef331863953f65e27954ca674bd073
Bartłomiej Sitnik <74111777+bartx3@users.noreply.github.com>
2024-06-25 01:37:42 +0200

    Update install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(valgrind" "clang-tidy" "clang" "clang-tools" "gcc" "cmake" "make" "python3" "zlib1g-dev" "libcunit1-dev")
+tools=("valgrind" "clang-tidy" "clang" "clang-tools" "gcc" "cmake" "make" "python3" "zlib1g-dev" "libcunit1-dev")
 
 blank=""
 
