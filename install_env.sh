892fd0b3e5ac6ff70fff0d6b795ee27c70e37e5c
Bartłomiej Sitnik <bartlomiej.sitnik.pwrrt@gmail.com>
2024-06-25 02:11:18 +0200

    Fixing checking installed dependencies

--- install_env.sh
+++ install_env.sh
@@ -4,13 +4,13 @@ echo "Installing required tools..."
 
 tools=("valgrind" "clang-tidy" "clang" "clang-tools" "gcc" "cmake" "make" "python3" "zlib1g-dev" "libcunit1-dev" "libcunit1")
 
-blank=""
+blank="(none)"
 
 for tool in "${tools[@]}"; do
-  policy=$(apt-cache policy "$tool" | grep "Installed:")
+  policy=$(apt-cache policy "$tool" | grep "Installed:" | awk -F': ' '{print $2}')
   echo "$tool" : "$policy"
   if [[ "$policy" == "$blank" ]]; then
   echo "Installing $tool..."
-    sudo apt-get install "$tool" -y
+    sudo apt-get install "$tool"
   fi
 done
