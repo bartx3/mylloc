06c9bcc9850d449e7bb5b8f05dc265293b71a30d
Bartłomiej Sitnik <bartlomiej.sitnik.pwrrt@gmail.com>
2024-06-25 04:57:51 +0200

    Fixing building

--- build_lib.sh
+++ build_lib.sh
@@ -1,7 +1,7 @@
 bash install_env.sh
 
 #if 2nd argument is clang then use clang compiler
-if [ "$2" == "clang" ]; then
+if [ "$2" = "clang" ]; then
   cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build # -S: source directory, -B: build directory
 else
   # otherwise use gcc compiler
